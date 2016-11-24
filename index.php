02d3e33e85eee5d2730f1cbc8c694b1cf153551f
Shahidul Islam <shawon_math07@yahoo.com>
2016-11-24 08:10:00 +0600

    basic scraping using curl

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<?php 
-$url='http://www.oooff.com';
-$output=file_get_contents($url);
-echo $output;
- ?>
+<?php
+$url = "oooff.com";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$curl_scraped_page = curl_exec($ch);
+curl_close($ch);
+echo $curl_scraped_page;
+?>
